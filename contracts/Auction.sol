3cd97dca191b6f5a7b97747b1eec78e6139ad518
admin <anandalex1993@gmail.com>
2024-06-16 23:35:14 +0530

    test completed

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.20;
 
 contract Auction {
 
+    address owner;
+
     struct AuctionItem {
         uint itemNumber;
-        uint stratingPrice;
+        uint startingPrice;
         uint duration;
         address highestBidder;
         uint bid;
@@ -27,7 +29,7 @@ contract Auction {
      * @param duration the duraction of the auction in seconds and cannot be zero
      */
 	function createAuction(uint256 itemNumber,uint256 startingPrice,uint256 duration) public {
-        require(owner == msg.sender);
+        require(owner == msg.sender, "Owner must be");
         require(auctions[itemNumber].itemNumber == 0, "Unique");
         require(startingPrice > 0, "startingPrice must be a positive number");
         require(duration > 0, "duration must be a positive number");
@@ -35,6 +37,8 @@ contract Auction {
         auctions[itemNumber].itemNumber = itemNumber;
         auctions[itemNumber].startingPrice = startingPrice;
         auctions[itemNumber].duration = block.timestamp + duration;
+        auctions[itemNumber].highestBidder = address(0);
+        auctions[itemNumber].bid = 0;
         auctions[itemNumber].active = true;
     }
 
@@ -47,14 +51,14 @@ contract Auction {
         AuctionItem storage auction = auctions[itemNumber];
         require(auction.active, "Auction is not active");
         require(block.timestamp < auction.duration, "Auction has ended");
-        require(msg.value > auction.highestBid, "Bid must be higher than current highest bid");
+        require(msg.value > auction.bid, "Bid must be higher than current highest bid");
 
         if (auction.highestBidder != address(0)) {
-            auction.highestBidder.transfer(auction.highestBid); // Refund previous highest bidder
+            payable(auction.highestBidder).transfer(bidAmount); // Refund previous highest bidder
         }
 
         auction.highestBidder = payable(msg.sender);
-        auction.highestBid = bidAmount;
+        auction.bid = bidAmount;
     }
 
     /**
@@ -63,14 +67,14 @@ contract Auction {
      * @param itemNumber the number of the item for which the auction is being cancelled
      */
 	function cancelAuction(uint256 itemNumber) public { 
-        Auctions storage auction = auctions[itemNumber];
+        AuctionItem storage auction = auctions[itemNumber];
         require(auction.active, "Auction is not active");
         require(block.timestamp < auction.duration, "Auction has ended");
 
-        auctions.active = false;
+        auction.active = false;
 
         if (auction.highestBidder != address(0)) {
-            auction.highestBidder.transfer(auction.highestBid); // Refund previous highest bidder
+            payable(auction.highestBidder).transfer(auction.bid); // Refund previous highest bidder
         }
     }
 
@@ -78,7 +82,7 @@ contract Auction {
      *  This function allows a user to check the status of an auction.
      * @param itemNumber the number of the item for which the auction status is being checked
      */
-    function checkAuctionActive(uint256 itemNumber) public returns (bool) { 
+    function checkAuctionActive(uint256 itemNumber) public view returns (bool) { 
         return auctions[itemNumber].active;
     }
 
@@ -87,7 +91,7 @@ contract Auction {
      * @param itemNumber the number of the item
      * @return time left in seconds for the auction for the given item Number
      */
-	function timeLeft(uint256 itemNumber) public returns (uint256) { 
+	function timeLeft(uint256 itemNumber) public view returns (uint256) { 
         return (auctions[itemNumber].duration - block.timestamp) / 60;
     }
 
@@ -97,7 +101,7 @@ contract Auction {
      * @return address  highest bidder if the auction is active or has ended, or 
      * address 0 if the auction has not started or has been cancelled.
      */
-	function checkHighestBidder(uint256 itemNumber) public returns (address) { 
+	function checkHighestBidder(uint256 itemNumber) public view returns (address) { 
         return auctions[itemNumber].highestBidder;
     }
 
@@ -107,7 +111,7 @@ contract Auction {
      * @param itemNumber the number of the item for which the owner is being checked
      * @return price highest bid price
      */
-	function checkActiveBidPrice(uint256 itemNumber) public returns (uint256){ 
+	function checkActiveBidPrice(uint256 itemNumber) public view returns (uint256){ 
         return auctions[itemNumber].bid;
     }
 
--- test/Auction/auction.ts
+++ test/Auction/auction.ts
@@ -6,7 +6,7 @@ import { ethers, network } from "hardhat";
 import { Signers } from "../types";
 import { deployLockFixture } from "./auction.fixture";
 
-describe("Roulette", function () {
+describe("Auction", function () {
   before(async function () {
     this.signers = {} as Signers;
 
@@ -16,141 +16,67 @@ describe("Roulette", function () {
     this.loadFixture = loadFixture;
   });
 
-  describe("test 1", function () {
+  describe("Auction Test", function () {
     beforeEach(async function () {
-      const { roulette, roulette_address, owner, address1, address2, address3 } =
+      const { auction,
+        auction_address, owner, address1, address2, address3 } =
         await this.loadFixture(deployLockFixture);
 
-      this.roulette = roulette;
-      this.roulette_address = roulette_address;
+      this.auction = auction;
+      this.auction_address = auction_address;
       this.owner = owner;
       this.address1 = address1;
       this.address2 = address2;
       this.address3 = address3;
     });
 
-    it("buyTokens", async function () {
-      const etherInWei = ethers.parseEther("1");
-      await this.roulette.connect(this.address1).buyTokens({ value: etherInWei });
-      await this.roulette.connect(this.address2).buyTokens({ value: etherInWei });
-      await this.roulette.connect(this.address3).buyTokens({ value: etherInWei });
+    it("createAuction", async function () {
+        const ethInWei = ethers.parseEther("10");
 
-      await this.roulette
-        .connect(this.address3)
-        .balanceOf(this.address3)
-        .then((balance: any) => {
-          expect(balance).to.equals(1000);
+        await this.auction.connect(this.owner).createAuction(1, ethInWei, 100);
+        await this.auction.connect(this.address1).checkAuctionActive(1).then((data: any) => {
+            expect(data).to.equal(true);
         });
     });
 
-    it('placeBet', async function () {
-        const etherInWei = ethers.parseEther("1");
+    it("bid", async function() {
+        const ethInWei = ethers.parseEther("11");
+        await this.auction.connect(this.owner).createAuction(1, ethInWei, 100);
+        await this.auction.connect(this.address1).bid(1, ethInWei, { value: ethInWei});
 
-        await this.roulette.connect(this.address1).buyTokens({ value: etherInWei });
-        await this.roulette.connect(this.address2).buyTokens({ value: etherInWei });
-        await this.roulette.connect(this.address3).buyTokens({ value: etherInWei });
-  
-        await this.roulette.connect(this.address1).placeBetEven(1000);
-        await this.roulette.connect(this.address2).placeBetOdd(1000);
-        await this.roulette.connect(this.address3).placeBetOnNumber(1000, 13);
-
-        await this.roulette.connect(this.address1).checkBetsOnEven().then((data: any) => {
-            expect(data[0].length).equals(1);
-        });
-
-        await this.roulette.connect(this.address2).checkBetsOnOdd().then((data: any) => {
-            expect(data[0].length).equals(1);
-
-        });
-
-        await this.roulette.connect(this.address3).checkBetsOnDigits().then((data: any) => {
-            expect(data[0].length).equals(1);
+        await this.auction.connect(this.address1).checkHighestBidder(1).then((data: any) => {
+            expect(this.address1).equals(data);
         });
     });
 
-    it("setSpinWheelResult", async function () {
-        const etherInWei = ethers.parseEther("1");
+    it("cancelAuction", async function() {
+        const ethInWei = ethers.parseEther("11");
+        await this.auction.connect(this.owner).createAuction(1, ethInWei, 100);
+        await this.auction.connect(this.address1).bid(1, ethInWei, { value: ethInWei});
 
-        await this.roulette.connect(this.address1).buyTokens({ value: etherInWei });
-        await this.roulette.connect(this.address2).buyTokens({ value: etherInWei });
-        await this.roulette.connect(this.address3).buyTokens({ value: etherInWei });
-  
-        await this.roulette.connect(this.address1).placeBetEven(1000);
-        await this.roulette.connect(this.address2).placeBetOdd(1000);
-        await this.roulette.connect(this.address3).placeBetOnNumber(1000, 13);
+        const address1BalanceBefore = await ethers.provider.getBalance(this.address1);
 
-        await this.roulette.connect(this.owner).setSpinWheelResult(13);
+        await this.auction.connect(this.owner).cancelAuction(1);
 
-        await this.roulette.connect(this.owner).checkWinningNumber().then((data: any) => {
-            expect(data).equals(13);
-        });
+        const address1BalanceAfter = await ethers.provider.getBalance(this.address1);
+
+        expect(address1BalanceBefore).to.lessThan(address1BalanceAfter);
     });
 
-    it("transferWinnings", async function () {
-        const etherInWei = ethers.parseEther("1");
+    it("timeLeft", async function() {
+        const ethInWei = ethers.parseEther("11");
+        await this.auction.connect(this.owner).createAuction(1, ethInWei, 10000);
 
-        await this.roulette.connect(this.address1).buyTokens({ value: etherInWei });
-        await this.roulette.connect(this.address2).buyTokens({ value: etherInWei });
-        await this.roulette.connect(this.address3).buyTokens({ value: etherInWei });
-  
-        await this.roulette.connect(this.address1).placeBetEven(1000);
-        await this.roulette.connect(this.address2).placeBetOdd(1000);
-        await this.roulette.connect(this.address3).placeBetOnNumber(1000, 13);
+        const ethInWei1 = ethers.parseEther("11");
+        await this.auction.connect(this.address1).bid(1, ethInWei1, { value: ethInWei1});
 
-        await this.roulette.connect(this.owner).setSpinWheelResult(13);
+        const ethInWei2 = ethers.parseEther("12");
+        await this.auction.connect(this.address1).bid(1, ethInWei2, { value: ethInWei2});
 
-        await this.roulette.connect(this.owner).transferWinnings();
-
-        await this.roulette.connect(this.address3).balanceOf(this.address3).then((data: any) => {
-            expect(data).equals(19000);
+        await this.auction.connect(this.owner).timeLeft(1).then((data: any) => {
+            expect(data).to.equal(166);
         });
 
-        await this.roulette.connect(this.address2).balanceOf(this.address2).then((data: any) => {
-            expect(data).equals(1800);
-        });
-
-        await this.roulette.connect(this.address1).balanceOf(this.address1).then((data: any) => {
-            expect(data).equals(0);
-        });
     });
-
-    it("sellTokens", async function () {
-        const etherInWei = ethers.parseEther("1");
-
-        await this.roulette.connect(this.address1).buyTokens({ value: etherInWei });
-        await this.roulette.connect(this.address2).buyTokens({ value: etherInWei });
-        await this.roulette.connect(this.address3).buyTokens({ value: etherInWei });
-  
-        await this.roulette.connect(this.address1).placeBetEven(1000);
-        await this.roulette.connect(this.address2).placeBetOdd(1000);
-        await this.roulette.connect(this.address3).placeBetOnNumber(1000, 13);
-
-        await this.roulette.connect(this.owner).setSpinWheelResult(13);
-
-        await this.roulette.connect(this.owner).transferWinnings();
-
-        await this.roulette.connect(this.address3).sellTokens(19000);
-        await this.roulette.connect(this.address2).sellTokens(1800);
-
-        await this.roulette.connect(this.address3).balanceOf(this.address3).then((data: any) => {
-            expect(data).equals(0);
-        });
-
-        await this.roulette.connect(this.address2).balanceOf(this.address2).then((data: any) => {
-            expect(data).equals(0);
-        });
-
-        await this.roulette.connect(this.address1).balanceOf(this.address1).then((data: any) => {
-            expect(data).equals(0);
-        });
-    });
-
-    it("spinWheel", async function () {
-        await this.roulette.connect(this.owner).spinWheel();
-
-        await this.roulette.connect(this.address1).checkWinningNumber().then((data: number) => {
-            expect(data).lessThanOrEqual(36);
-        })
-    })
   });
 });
